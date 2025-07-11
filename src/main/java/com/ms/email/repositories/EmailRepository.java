b9e05407ffd53c943e6809084d2c9864e6a58d8b
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-07-10 21:39:10 -0300

    Add queue, message converter and consumer

--- src/main/java/com/ms/email/repositories/EmailRepository.java
+++ src/main/java/com/ms/email/repositories/EmailRepository.java
@@ -1,7 +1,10 @@
 package com.ms.email.repositories;
 
 import com.ms.email.models.EmailModel;
+
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 }
