6c1fb3e6a691f9e26ed18e76ec5706eabd8cffb1
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-07-13 13:54:43 -0300

    Add endpoints

--- src/main/java/com/ms/email/controllers/EmailController.java
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -4,19 +4,30 @@ import com.ms.email.dtos.EmailDto;
 import com.ms.email.models.EmailModel;
 import com.ms.email.services.EmailService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @RestController
 public class EmailController {
 
-    @Autowired
-    EmailService emailService;
+    private final EmailService emailService;
 
     @PostMapping("/sending-email")
     public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto){
@@ -25,4 +36,27 @@ public class EmailController {
         emailService.sendEmail(emailModel);
         return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
     }
+
+    @GetMapping("/emails")
+    public ResponseEntity<Page<EmailModel>> getEmails(@PageableDefault(page = 0, size = 5, sort = "emailId", direction = Sort.Direction.DESC) Pageable pageable) {
+        return new ResponseEntity<>(emailService.listAll(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping("/emails/all")
+    public ResponseEntity<List<EmailModel>> getAllEmails() {
+        return new ResponseEntity<>(emailService.listAllNonPageable(), HttpStatus.OK);
+    }
+    
+    
+    @GetMapping("/emails/{emailId}")
+    public ResponseEntity<Object> getEmailById(@PathVariable UUID emailId) {
+        Optional<EmailModel> emailModelOptional = emailService.findByEmailId(emailId);
+
+        if(!emailModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email not found.");
+        } else{
+            return ResponseEntity.status(HttpStatus.OK).body(emailModelOptional.get());
+        }
+    }
+    
 }
--- src/main/java/com/ms/email/services/EmailService.java
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -3,23 +3,31 @@ package com.ms.email.services;
 import com.ms.email.enums.StatusEmail;
 import com.ms.email.models.EmailModel;
 import com.ms.email.repositories.EmailRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+@RequiredArgsConstructor
 @Service
 public class EmailService {
 
-    @Autowired
-    private EmailRepository emailRepository;
+    private final EmailRepository emailRepository;
 
-    @Autowired
-    private JavaMailSender emailSender;
+    private final JavaMailSender emailSender;
 
+    @Transactional
     public EmailModel sendEmail(EmailModel emailModel) {
         emailModel.setSendDateEmail(LocalDateTime.now());
 
@@ -38,4 +46,17 @@ public class EmailService {
 
         return emailRepository.save(emailModel);
     }
+
+    public Page<EmailModel> listAll(Pageable pageable){
+        return emailRepository.findAll(pageable);
+    }
+
+    public List<EmailModel> listAllNonPageable(){
+        return emailRepository.findAll();
+    }
+
+    public Optional<EmailModel> findByEmailId(UUID emailId){
+        return emailRepository.findById(emailId);
+    }
+    
 }
